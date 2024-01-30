e88f26a2460fe354b15f055efa97461429713192
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-01-30 17:06:29 +0400

    added main_menu interface

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -3,6 +3,8 @@ from aiogram import Bot, Dispatcher, Router, F, types
 from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
 from datetime import datetime
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from werkzeug.security import generate_password_hash, check_password_hash
 from django.core.cache import cache
 
@@ -39,3 +41,21 @@ async def start(clb: CallbackQuery):
         cache.set(key=cache_key, value={"email": user.email, "token": token, "expires_in": expires_in}, timeout=expires_in)
 
     await main_menu(clb)
+
+
+@router.callback_query(F.data == "main_menu")
+async def main_menu(clb: CallbackQuery):
+    builder = InlineKeyboardBuilder()
+
+    builder.row(InlineKeyboardButton(text='Модели', callback_data='select_model'))
+    builder.row(InlineKeyboardButton(text='Профиль', callback_data='account'))
+    builder.row(InlineKeyboardButton(text='Помощь', callback_data='help'))
+
+    await clb.answer(
+        text="""
+        Добро пожаловать в Bot-X!\n
+Я ваш личный телеграмм-помощник, который облегчит вам жизнь, отвечая на вопросы и поддерживая с вами общение.\n
+Чтобы начать диалог, выберите одну из моделей ниже и задайте интересующий вас вопрос.
+        """,
+        reply_markup=builder.as_markup()
+    )
