7f4684450e6a8082f2694afcc7071c52cf9ee008
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-02-02 20:22:18 +0400

    func add

--- telegram_bot/run.py
+++ telegram_bot/run.py
@@ -1,7 +1,10 @@
 import os, django, asyncio, logging
-from aiogram import Dispatcher, Bot
+from aiogram import Dispatcher, Bot, F
 from views import router
-
+from start import start
+from views import main_menu
+from help import help
+from select_model import select_model
 ''' django setup '''
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
@@ -13,6 +16,10 @@ async def main() -> None:
     bot = Bot(token=token, parse_mode='HTML')
     dp = Dispatcher()
     dp.include_router(router=router)
+    dp.message.register(start)
+    dp.message.register(main_menu)
+    dp.message.register(help)
+    dp.message.register(select_model)
     await dp.start_polling(bot, skip_updates=True)
 
 
