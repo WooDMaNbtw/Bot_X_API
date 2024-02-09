94d43caf602069b4a1782ade79831fc46871c803
Stogov Egor <severeff@gmail.com>
2024-02-09 14:41:39 +0100

    изменен файл запуска

--- telegram_bot/run.py
+++ telegram_bot/run.py
@@ -1,14 +1,16 @@
 import os, django, asyncio, logging
-from aiogram import Dispatcher, Bot, F
-from views import router
-from start import start
-from views import main_menu
-from help import help
-from select_model import select_model
+from aiogram import Dispatcher, Bot
+
+import sys
+sys.path.append(r'D:\devops\bot-x-tele\Bot_X_API')
+
 ''' django setup '''
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bot_X_API.core.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
+
+from tg_services.assets import router
+from tg_services import help, select_model, start, main_menu
 
 
 async def main() -> None:
@@ -16,10 +18,10 @@ async def main() -> None:
     bot = Bot(token=token, parse_mode='HTML')
     dp = Dispatcher()
     dp.include_router(router=router)
-    dp.message.register(start)
-    dp.message.register(main_menu)
-    dp.message.register(help)
-    dp.message.register(select_model)
+    dp.message.register(start.start)
+    dp.message.register(main_menu.main_menu)
+    dp.message.register(help.help)
+    dp.message.register(select_model.select_model)
     await dp.start_polling(bot, skip_updates=True)
 
 
